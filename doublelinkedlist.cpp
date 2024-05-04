c0ff30cf5654638b644176db867ab21afcf753ec
naufaalkh <naufaalkh@gmail.com>
2024-05-04 21:27:47 +0700

    kondisi penghapusan saat node yang akan dihapus adalah node pertama

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -112,3 +112,13 @@ void deleteNode()
         cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
         return;
     }
+
+    // Node to be deleted is the first node
+    if (current == START)
+    {
+        START = START->next; // step 2: update the START pointer
+        if (START != NULL)
+        {                       //
+            START->prev = NULL; // step
+        }
+    }

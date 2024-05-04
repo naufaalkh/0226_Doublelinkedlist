24a3e8bcfbb0921a98f36c76dfbc54d314f146bb
naufaalkh <naufaalkh@gmail.com>
2024-05-04 21:24:17 +0700

    menambahkan proses pencarian dalam fungsi hapus node

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -96,3 +96,13 @@ void deleteNode()
         cout << "List is empty" << endl;
         return;
     }
+    
+    current = START; // step 1: start from the first node
+    previous = NULL;
+
+    // Locate the node to be deleted
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
